039889f31c38e976947dd8534c5173c10fc34d3e
Parker Jones <parker.matthewjones@gmail.com>
2023-08-11 21:45:42 -0500

    show working example with react-intersection-observer

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import { type V2_MetaFunction, type LoaderArgs, defer } from "@remix-run/node";
 import { Await, useLoaderData } from "@remix-run/react";
 import { motion, useInView } from "framer-motion";
+import { useInView as observerUseInView } from "react-intersection-observer";
 import { Suspense, useEffect, useRef } from "react";
 
 export const meta: V2_MetaFunction = () => {
@@ -12,22 +13,28 @@ export const meta: V2_MetaFunction = () => {
 
 export const loader = async ({ request }: LoaderArgs) => {
   const pikachuQuery = fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+  const charmanderQuery = fetch("https://pokeapi.co/api/v2/pokemon/charmander");
   const dittoQuery = new Promise((resolve) => {
     setTimeout(() => {
       resolve(fetch("https://pokeapi.co/api/v2/pokemon/ditto"));
+    }, 1000);
+  });
+  const squirtleQuery = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(fetch("https://pokeapi.co/api/v2/pokemon/squirtle"));
     }, 2000);
   });
-  const charmanderQuery = fetch("https://pokeapi.co/api/v2/pokemon/charmander");
 
   const pikachu = await pikachuQuery.then((res) => res.json());
   const charmander = charmanderQuery.then((res) => res.json());
   const ditto = dittoQuery.then((res: any) => res.json());
+  const squirtle = squirtleQuery.then((res: any) => res.json());
 
-  return defer({ pikachu, charmander, ditto });
+  return defer({ pikachu, charmander, ditto, squirtle });
 };
 
 export default function Index() {
-  const { pikachu, charmander, ditto } = useLoaderData();
+  const { pikachu, charmander, ditto, squirtle } = useLoaderData();
 
   const pikachuRef = useRef(null);
   const pikachuIsInView = useInView(pikachuRef, {
@@ -53,9 +60,17 @@ export default function Index() {
     console.log("dittoIsInView", dittoIsInView);
   }, [dittoIsInView]);
 
+  const { ref: squirtleRef, inView: squirtleIsInView } = observerUseInView({
+    triggerOnce: true,
+  });
+  useEffect(() => {
+    console.log("squirtleIsInView", squirtleIsInView);
+  }, [squirtleIsInView]);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Pokemon</h1>
+      {/* no suspense */}
       <motion.div
         ref={pikachuRef}
         animate={pikachuIsInView ? { translateX: 0 } : { translateX: 300 }}
@@ -64,6 +79,7 @@ export default function Index() {
         <h2>{pikachu.name}</h2>
         <img src={pikachu.sprites.front_default} alt="pikachu" />
       </motion.div>
+      {/* suspense with non-delayed response */}
       <Suspense fallback={<div>Loading...</div>}>
         <Await resolve={charmander}>
           {(charmander: any) => (
@@ -80,6 +96,7 @@ export default function Index() {
           )}
         </Await>
       </Suspense>
+      {/* suspense with delayed response */}
       <Suspense fallback={<div>Loading...</div>}>
         <Await resolve={ditto}>
           {(ditto: any) => (
@@ -90,6 +107,23 @@ export default function Index() {
             >
               <h2>{ditto.name}</h2>
               <img src={ditto.sprites.front_default} alt="ditto" />
+            </motion.div>
+          )}
+        </Await>
+      </Suspense>
+      {/* using react-intersection-observer with a delayed response */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Await resolve={squirtle}>
+          {(squirtle: any) => (
+            <motion.div
+              ref={squirtleRef}
+              animate={
+                squirtleIsInView ? { translateX: 0 } : { translateX: 300 }
+              }
+              transition={{ duration: 0.25 }}
+            >
+              <h2>{squirtle.name}</h2>
+              <img src={squirtle.sprites.front_default} alt="squirtle" />
             </motion.div>
           )}
         </Await>
